1616344b3942d8a89a742f90cfca5b6a817cc943
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-05-12 19:50:58 -0300

    create user functions
    added login, user create, user delete methods

--- user/repositories/repository.py
+++ user/repositories/repository.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 import pymongo
+from bson import ObjectId
+from django.contrib.auth.hashers import check_password
  
 class UserRepository:
     collection_name = 'users'
@@ -17,38 +19,55 @@ class UserRepository:
         collection = self.get_collection()
         users = collection.find({})
         return list(users)
-   
-    def get_user_by_email(self, email):
+    
+    def get_by_email(self, email=None):
+        query = {}
+        if email:
+            query['email'] = email
+        
+        documents = self.get_collection().find(query)
+        return list(documents)
+    
+    def get_hash(self):
         collection = self.get_collection()
-        user = collection.find_one({"email": email})
-        return user
+
    
     def create_user(self, user_data):
         collection = self.get_collection()
         result = collection.insert_one(user_data)
         return result.inserted_id
- 
-    def check_password(self, user_email, entered_password):
-        collection = self.get_collection()
-        user = collection.find_one({"email": user_email})
- 
-        if user and user.get('password'):
-            return user['password'] == entered_password
-        return False
- 
-    def update_user(self, email, update_data):
-        collection = self.get_collection()
-        result = collection.update_one({"email": email}, {"$set": update_data})
-        return result.modified_count
    
-    def delete_user(self, email):
-        collection = self.get_collection()
-        result = collection.delete_one({"email": email})
-        return result.deleted_count
+    def delete(self, document_id):
+        self.get_collection().delete_one({'_id': ObjectId(document_id)})
+
+    def update(self, document_id, new_user_data):
+        self.get_collection().update_one({'_id': ObjectId(document_id)}, {'$set': new_user_data})
    
+    # def authenticate_user(self, email, password):
+    #     user = self.get_by_email(email=email)
+        
+    #     if user:
+    #         # Obter a senha armazenada do usuário no banco de dados
+    #         stored_password = user[0]['password']
+
+    #         print(f'Email do cadastro: {email}')
+    #         print(f'Hash do cadastro: {stored_password}')
+            
+    #         # Comparar a senha fornecida com a senha armazenada usando check_password
+    #         if check_password(password, stored_password):
+    #             return user[0]
+        
+    #     return None
+
     def authenticate_user(self, email, password):
-        user = self.get_user_by_email(email)
-        if user and self.check_password(password, user.get('password', '')):
-            return True, user
-        else:
-            return False, None  
+        user = self.get_by_email(email=email)
+        
+        if user:
+            # Obter a senha armazenada do usuário no banco de dados
+            stored_password = user[0]['password']
+            
+            # Comparar a senha fornecida com a senha armazenada usando check_password
+            if check_password(password, stored_password):
+                return user[0]
+        
+        return None
--- user/views/views.py
+++ user/views/views.py
@@ -1,15 +1,18 @@
-import os
-import jwt
+import hashlib
+from pyexpat.errors import messages
+from urllib import response
+from django.urls import reverse
 import logging
-from datetime import datetime, timedelta
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate 
 from django.views import View
 from django.contrib.auth.hashers import make_password
 from user.repositories.repository import UserRepository
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from uuid import uuid4
+from django.contrib import messages
  
 # Configuração do logger
 logging.basicConfig(level=logging.INFO)
@@ -34,7 +37,111 @@ class UserRegister(View):
             if not email or '@' not in email or '.' not in email:
                 raise ValueError('Por favor, insira um email válido.')
  
-            existing_user = user_repo.get_user_by_email(email=email)
+            existing_user = user_repo.get_by_email(email=email)
+            if existing_user:
+                raise ValueError('Este email já está em uso. Por favor, escolha outro.')
+ 
+            # Gerar o hash da senha 
+            hashed_password = make_password(password)
+ 
+            new_user_data = {
+                'uuid': str(uuid4()),
+                'email': email,
+                'username': username,
+                'password': hashed_password,
+            }
+            user_repo.create_user(new_user_data)
+ 
+            return HttpResponseRedirect(reverse('user-list'))
+        except Exception as e:
+            logger.error(f'Erro ao registrar usuário: {e}')
+            return render(request, 'base.html')
+        
+
+class UserList(View):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: HttpRequest):
+        try:
+            # Obtenha a lista de usuários do repositório
+            users = UserRepository().get_all_users()
+
+            # Converta o ObjectId para string se necessário
+            for user in users:
+                user['id'] = str(user['_id'])
+
+            return render(request, "user_list.html", {"weather_records": users})
+
+        except Exception as e:
+            return HttpResponse(content="Erro ao obter os dados dos usuários.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UserListField(View):
+    def get(self, request, **kwargs):
+        email = kwargs.get('email')
+
+        # Instancia o repositório do clima
+        repository = UserRepository()
+
+        try:
+            # Busca os registros do clima para a cidade especificada
+            if email:
+                user_data = repository.get_by_email(email=email)
+            else:
+                user_data = repository.get_by_email()
+
+            # Adiciona o campo 'id' aos objetos retornados
+            for item in user_data:
+                item['id'] = str(item['_id'])
+
+            # Renderiza o template weather-list.html com os dados serializados
+            return render(request, 'user_field.html', {'user_records': user_data})
+        
+        except Exception as e:
+            return response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UserDelete(View):
+     def get(self, request, pk):
+        repository = UserRepository()
+        try:
+            repository.delete(pk)
+            return HttpResponseRedirect(reverse('user-list'))
+        except Exception as e:
+            return HttpResponseBadRequest(f"Erro ao excluir registro: {str(e)}")
+        
+
+class UserGetView(View):
+    def get(self, request, pk):
+        repository = UserRepository()
+        try:
+            user_data = repository.get(pk)
+            if user_data:
+                return render(request, 'user_update.html', {'user': user_data, 'pk': pk})  
+            else:
+                return HttpResponseBadRequest(f"Registro não encontrado para o ID: {pk}")
+        except Exception as e:
+            return HttpResponseBadRequest(f"Erro ao recuperar registro: {str(e)}")
+
+
+class WeatherUpdateView(View):
+    def post(self, request, pk):
+
+        repository = UserRepository()
+
+        try:
+            email = request.POST.get('email')
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+ 
+            user_repo = UserRepository()
+ 
+            # Validação do email
+            if not email or '@' not in email or '.' not in email:
+                raise ValueError('Por favor, insira um email válido.')
+ 
+            existing_user = user_repo.get_by_email(email=email)
             if existing_user:
                 raise ValueError('Este email já está em uso. Por favor, escolha outro.')
  
@@ -46,36 +153,36 @@ class UserRegister(View):
                 'username': username,
                 'password': hashed_password,
             }
-            user_repo.create_user(new_user_data)
- 
-            return render(request, 'base.html')
+
+                # Atualizar no MongoDB
+            repository.update(pk, new_user_data)
+
+            return HttpResponseRedirect(reverse('user-list'))
         except Exception as e:
-            logger.error(f'Erro ao registrar usuário: {e}')
-            return render(request, 'base.html')
- 
-class UserLogin(View):
+            return HttpResponseBadRequest(f"Erro ao atualizar registro: {str(e)}")
+
+class Login(View):
     def get(self, request):
         return render(request, 'login.html')
- 
+
     def post(self, request):
-        try:
-            email = request.POST.get('email')
-            password = request.POST.get('password')
- 
-            # Chame a função authenticate para autenticar o usuário
-            user = authenticate(request, username=email, password=password)
- 
-            if user is not None:
-                # Autenticação bem-sucedida, gerando tokens
-                refresh = RefreshToken.for_user(user)
-                access_token = str(refresh.access_token)
- 
-                # Logging do login bem-sucedido
-                logger.info(f'Usuário {user.username} fez login com sucesso.')
- 
-                return render(request, 'main.html')
-            else:
-                raise ValueError('Credenciais inválidas. Por favor, tente novamente.')
-        except Exception as e:
-            logger.error(f'Erro ao fazer login: {e}')
-            return redirect('login')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Autenticar o usuário
+        user_repo = UserRepository()
+        user = user_repo.authenticate_user(email, password)
+
+        if user:
+            # Autenticação bem-sucedida, redirecione para alguma página
+            return redirect('weather-list')
+        else:
+            # Autenticação falhou
+            print('A autenticação falhou')
+            messages.error(request, 'Email ou senha incorretos.')
+
+        return render(request, 'login.html')
+    
+
+class UserUpdate(View):
+    pass
