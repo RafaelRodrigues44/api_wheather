abb33ad294937d04dcd4ced2e7e95838af6c5e7f
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-05-15 02:44:23 -0300

    fix(home): fixed home bug

--- api_tempo/view/renderView.py
+++ api_tempo/view/renderView.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     user = None
-    if request.user.is_authenticated:
+    if request.user:
         user = request.user
     return render(request, 'base.html', {'user': user})
